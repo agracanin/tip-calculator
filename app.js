24f269dec59a3d156e171fed3c646c53063a4e89
agracanin <gracana2@sunypoly.edu>
2023-11-01 16:51:36 -0400

    styles updated; js core functions

---
+++ app.js
@@ -0,0 +1,24 @@
+const initialPrice = document.getElementById('bill-price');
+const finalPrice = document.getElementById('total-amount');
+const tipCalculation = document.getElementById('tip-amount');
+const people = document.getElementById('people');
+
+// initialPrice.addEventListener
+
+// people.addEventListener 
+
+
+function calculateFinal(bill, people, tip) {
+    let final = 0;
+
+    if (bill !== 0, people !== 0, tip !== 0) {
+        tip = bill * tip
+        bill += tip
+        final = bill / people
+    }
+}
+
+function render(price, tip) {
+
+}
+
