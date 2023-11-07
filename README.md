72d6e8f1988bd2f48d2b79688af7b5decb250587
Alen <alengracanin@gmail.com>
2023-11-07 14:21:12 -0500

    Completed all JS calculations; Active state styles

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# tip-calculator
+# Tip Calculator | [View Live](https://agracanin.github.io/tip-calculator/)
+
+A challenge from [Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX) to practice creating designs from UI
+design files.
+
+## Features
+- Calculate per person amount for a food bill with a tip included
+- Allows for custom tip inputs as well as some common defaults
+- Reset functionality
+
+## Demo
+https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX
--- app.js
+++ app.js
@@ -1,24 +1,110 @@
-const initialPrice = document.getElementById('bill-price');
+const billInput = document.getElementById('bill-price');
 const finalPrice = document.getElementById('total-amount');
 const tipCalculation = document.getElementById('tip-amount');
-const people = document.getElementById('people');
+const peopleInput = document.getElementById('people');
+const tipSelectors = document.querySelectorAll("#tip-selector");
+const customTip = document.getElementById('custom-tip');
+const resetBtn = document.getElementById('reset');
 
-// initialPrice.addEventListener
+let billTemp = 0;
+let peopleTemp = 0;
+let tipTemp = 0.15;
 
-// people.addEventListener 
+resetBtn.addEventListener('click', reset)
 
+billInput.addEventListener('blur', (e) => {
+    if (billInput.value.includes('.') && billInput.value.split('.')[1].length > 2) {
+        e.target.value = billInput.value.slice(0, billInput.value.indexOf('.') + 3);
+    }
+
+    setValues(billInput.value, tipTemp, peopleInput.value)
+    return;
+})
+
+peopleInput.addEventListener('blur', (e) => {
+    setValues(billInput.value, tipTemp, peopleInput.value);
+    return;
+})
+
+tipSelectors.forEach(selector => {
+    selector.addEventListener('click', (e) => {
+        tipSelectors.forEach(el => {
+            el.classList.remove('active');
+        });
+        customTip.classList.remove('input-active');
+
+        selector.classList.add('active');
+        tipTemp = parseFloat(selector.dataset.tip / 100);
+        setValues(billInput.value, tipTemp, peopleInput.value);
+    })
+})
+
+customTip.addEventListener('click', () => {
+    tipSelectors.forEach(selector => {
+        selector.classList.remove('active');
+    })
+
+    customTip.classList.add('input-active');
+})
+
+customTip.addEventListener('blur', (e) => {
+    let value = parseInt(customTip.value)
+    if (value > 100) {
+        value = 100;
+    } else if (value <= 0) {
+        value = 1;
+        customTip.value = value;
+    }
+
+    tipTemp = value / 100;
+    setValues(billTemp, tipTemp, peopleTemp);
+})
+
+
+function setValues(bill, tip, people) {
+    billTemp = parseFloat(bill);
+    tipTemp = parseFloat(tip);
+    peopleTemp = parseInt(people);
+
+    if (billTemp > 0 && tip > 0 && people > 0) {
+        calculateFinal(billTemp, peopleTemp, tipTemp);
+    }
+
+    return;
+}
 
 function calculateFinal(bill, people, tip) {
     let final = 0;
 
-    if (bill !== 0, people !== 0, tip !== 0) {
-        tip = bill * tip
-        bill += tip
-        final = bill / people
-    }
+    tip = bill * tip
+    bill += tip
+    final = bill / people
+    tip = tip / people
+
+    render(final, tip);
+    return;
 }
 
 function render(price, tip) {
+    tipCalculation.textContent = `$${parseFloat(tip).toFixed(2)}`;
+    finalPrice.textContent = `$${parseFloat(price).toFixed(2)}`;
+    return;
+}
 
+function reset() {
+    billTemp = 0;
+    tipTemp = 0;
+    tipTemp = 0;
+    tipCalculation.textContent = "$0.00";
+    finalPrice.textContent = `$0.00`;
+    customTip.value = "";
+    peopleInput.value = "";
+    billInput.value = "";
+
+    customTip.classList.remove('input-active');
+
+    tipSelectors.forEach(selector => {
+        selector.classList.remove('active');
+    })
 }
 
